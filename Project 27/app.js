bd8ec748e9a1e8b3ad52b0a90ca4c758e90a7548
Gromovoi Stanislav <gromila12@rambler.ru>
2023-12-25 16:43:32 +0600

    Project 27

--- Project 27/app.js
+++ Project 27/app.js
@@ -12,18 +12,18 @@ async function sendMessage() {
   await channel.assertQueue(queue, {
     durable: false,
 
-    // messageTtl: 1000 * 60, // Установка TTL сообщений по умолчанию
-    // expires: 1000 * 60*2, // Установка TTL самой очереди
+    // messageTtl: 1000 * 60, // Установка TTL сообщений по умолчанию в миллисекундах (60 секунд)
+    // expires: 1000 * 60*2, // Установка TTL самой очереди в миллисекундах (2 мин)
 
     // Или:
     arguments: {
-      'x-message-ttl': 1000 * 60, // Установка TTL сообщений по умолчанию
-      'x-expires': 1000 * 60 * 2, // Установка TTL самой очереди
+      'x-message-ttl': 1000 * 60, // Установка TTL сообщений по умолчанию в миллисекундах (60 секунд)
+      'x-expires': 1000 * 60 * 2, // Установка TTL самой очереди в миллисекундах (2 мин)
     },
   });
 
   // Установка TTL для конкретного сообщения
-  const expirationTimeInMillis = 1000 * 10; // Время жизни сообщения в миллисекундах (например, 10 секунд)
+  const expirationTimeInMillis = 1000 * 30; // Установка TTL конкретного сообщения в миллисекундах (30 секунд)
   channel.sendToQueue(queue, Buffer.from(message), {
     expiration: expirationTimeInMillis,
   });
